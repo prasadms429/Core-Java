5fd72c151bc0413b2702fc8a44c6af7762202998
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-28 09:57:29 +0530

    Update User_input.java

--- Introducing java/User_input.java
+++ Introducing java/User_input.java
@@ -6,6 +6,7 @@ public class KeyboardInput			// In this 'KeyboardInput' is class name, it must b
 	    String name;
 	    int age;
 	    String city;
+	    double num;
 	    
 		System.out.println("What is your name ?");
 		name=keyboard.nextLine();         // Due to 'nexline' method, we can enter as many as words, character until we press nextline or Enter key in keyboard.
@@ -17,11 +18,17 @@ public class KeyboardInput			// In this 'KeyboardInput' is class name, it must b
 		//but it in 'nextInt()' method it leaves nexline as it is, so sometimes the it will not prompt user to enter their input, without that it prints output, 
 		//So we have to enter "nextLine()" method after any non string methods like 'nextInt' or 'nextFloat' etc.
 		
+		System.out.println("Enter the number: "); // Program to double the entered number
+		num=keyboard.nextDouble();
+		keyboard.nextLine();
+		num*=2;
+		
 		System.out.println("What is your city ?");
 		city=keyboard.nextLine();
 		
 		System.out.println("Your name "+name);
 		System.out.println("Your age "+age);
+		System.out.println("The doubled value "+num);
 		System.out.println("Your city "+city);
 	}
 }
