5f954d9fe535016e96f70087b97bd6f1c280c0f6
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-28 12:28:13 +0530

    Update AverageOfThree.java

--- Introducing java/AverageOfThree.java
+++ Introducing java/AverageOfThree.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class AverageOfThree
+{
+	public static void main(String[] args) {
+		double num1,num2,num3,Average;
+	    Scanner keyboard=new Scanner(System.in);
+	    
+		System.out.println("Enter first number:");
+		num1=keyboard.nextDouble();
+		keyboard.nextLine();
+		
+		System.out.println("Enter second number:");
+		num2=keyboard.nextDouble();
+		keyboard.nextLine();
+		
+		System.out.println("Enter third number:");
+		num3=keyboard.nextDouble();
+		keyboard.nextLine();
+		
+		Average=(num1+num2+num3)/3.0;
+		
+		System.out.println("The Average is "+Average);
+	}
+}
