7701cb828c0ddd89bab863f3f42b14c520dabc6c
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-28 09:14:18 +0530

    Update User_input.java

--- Introducing java/User_input.java
+++ Introducing java/User_input.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class KeyboardInput
+public class KeyboardInput			// In this 'KeyboardInput' is class name, it must be same with file name given.
 {
 	public static void main(String[] args) {
-	    Scanner keyboard=new Scanner(System.in);    -- Scanner is to scan the console and System.in to read input given.
+	    Scanner keyboard=new Scanner(System.in);    // 'Scanner' is to scan the console and 'System.in' to read input given.
 	    String name;
 	    
 		System.out.println("What is your name ?");
-		name=keyboard.nextLine();         -- Due to nexline method, we can enter as many as words, character until we press nextline or Enter key in keyboard.
+		name=keyboard.nextLine();         // Due to 'nexline' method, we can enter as many as words, character until we press nextline or Enter key in keyboard.
 		System.out.println("Your name "+name);
 	}
 }
