b028f0c6a945c3707f7f88f1e683f3d477af7ddc
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-30 19:34:28 +0530

    Update 4) Random_number.java

--- Control statements/4) Random_number.java
+++ Control statements/4) Random_number.java
@@ -11,7 +11,7 @@ public class RandomFun
 		myRandomNumber=random.nextInt(1000);                    // prints random number between 0 t0 999
 		System.out.println("number is \t"+myRandomNumber);
 		
-		myRandomNumber=random.nextInt(1000)+1;                 // prints random number between 1 t0 1000
+		myRandomNumber=random.nextInt(1000)+1;                 // prints random number between 1 t0 1000  , This is also called shifting technique.
 		System.out.println("number is \t"+myRandomNumber);
 		
 	}
