cab635a81ff82c38da424353f43ae70dfda8bb68
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-30 19:39:55 +0530

    Update 4) Random_number.java

--- Control statements/4) Random_number.java
+++ Control statements/4) Random_number.java
@@ -16,3 +16,20 @@ public class RandomFun
 		
 	}
 }
+
+
+import java.util.Random;
+public class DiceSimulation
+{
+	public static void main(String[] args) {
+	    Random random= new Random();
+	    int diceVal;
+	    
+	   for(int i=0;i<10;i++)
+	   {
+	       diceVal=random.nextInt(6)+1;
+	       System.out.println("The dice number is "+diceVal);
+	   }
+		
+	}
+}
