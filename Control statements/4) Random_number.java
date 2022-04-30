cc2830387cf436be72c301d6db0b49df68d9edb2
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-30 19:23:07 +0530

    Create 4) Random_number.java

---
+++ Control statements/4) Random_number.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class RandomFun
+{
+	public static void main(String[] args) {
+	    Random random= new Random();
+	    int myRandomNumber;
+	    
+	    myRandomNumber=random.nextInt();
+		System.out.println("number is \t"+myRandomNumber);
+		
+		myRandomNumber=random.nextInt(1000);                    // prints random number between 0 t0 999
+		System.out.println("number is \t"+myRandomNumber);
+		
+		myRandomNumber=random.nextInt(1000)+1;                 // prints random number between 1 t0 1000
+		System.out.println("number is \t"+myRandomNumber);
+		
+	}
+}
