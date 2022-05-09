bbec163ffb231ca0921e8d12783b320a2a409a3a
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-09 18:40:01 +0530

    Create StringBuilderClass.java

---
+++ 4)Strings and String Builder class/StringBuilderClass.java
@@ -0,0 +1,19 @@
+public class StringBuilderFun
+{
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder("John Baugh");   // String Builder alters the stored object, withought replacing its reference. (The string stores reference of object, when we try to modify string, it creates new object and assign its address, leaving original object as it is in memory. ) 
+		
+		sb.append(" is awesome");
+		System.out.println(sb);
+		
+		sb.insert(5,"Philips ");
+		System.out.println(sb);
+		
+		sb.replace(22,29,"amazing");
+		System.out.println(sb);
+		
+		sb.delete(5,13);
+		System.out.println(sb);
+		
+	}
+}
