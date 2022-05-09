7ca87867814187625e53005c70e5438da5c310a6
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-09 17:07:24 +0530

    Create String Methods Part2.java

---
+++ 4)Strings and String Builder class/String Methods Part2.java
@@ -0,0 +1,17 @@
+public class StringMethods2
+{
+	public static void main(String[] args) {
+		String name="John Baugh";
+		
+		String upper=name.toUpperCase();
+		String lower=name.toLowerCase();
+		int whereIsB=name.indexOf("B");
+		String lastName=name.substring(5);
+		
+		System.out.println("Upper is "+upper);
+		System.out.println("Lower is "+lower);
+		System.out.println("B is at index of "+whereIsB);
+		System.out.println("Last name is "+lastName);
+		
+	}
+}
