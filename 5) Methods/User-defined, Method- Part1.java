ac18bbe9050b5de65d493bf5426a7125e28b2b96
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-10 12:36:24 +0530

    Create User-defined, Method- Part1.java

---
+++ 5) Methods/User-defined, Method- Part1.java
@@ -0,0 +1,44 @@
+public class Main
+{
+	public static void main(String[] args) {
+		printHello();
+		printNumber(5);
+		
+		int result= giveMe10();
+		System.out.println(result);
+		
+		result=addTheseTwo(10,10);
+		System.out.println(result);
+		
+		double dresult;
+		dresult=square(3);
+		System.out.println(dresult);
+		// or
+		System.out.println(square(3));
+	}
+	
+	//void, parameterless
+	public static void printHello(){
+	    System.out.println("Hello there!");
+	}
+	
+	// void, parameterized
+	public static void printNumber(int a){
+	    System.out.println("The number is "+a);
+	}
+	
+	// return, parameterless
+	public static int giveMe10()
+	{
+	    return 10;
+	}
+	
+	// return, parameterized
+	public static int addTheseTwo(int num1,int num2){
+	    return num1+num2;
+	}
+	
+	public static double square(double a){
+	    return a*a;
+	}
+}
