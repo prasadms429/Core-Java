d69e5661fe5f84443fda61d19703dd021563670c
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-10 21:41:18 +0530

    Update Project_SumOfElements.java

--- 5) Methods/Project_SumOfElements.java
+++ 5) Methods/Project_SumOfElements.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class sumOfElements
 {
 	public static void main(String[] args) {
+		
 	    ArrayList<Integer> someList=new ArrayList<>();
 	    someList.add(1);
 	    someList.add(2);
