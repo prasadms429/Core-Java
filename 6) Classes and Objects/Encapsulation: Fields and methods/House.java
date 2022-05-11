e6219b7548e261e20fa7a29333b34368032f2cd4
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-11 19:32:42 +0530

    Create House.java

---
+++ 6) Classes and Objects/Encapsulation: Fields and methods/House.java
@@ -0,0 +1,51 @@
+public class House      // Object is an entity that contains Behavior and data. Encapsulation contains methods and data in it.
+{
+    private int numStories;     // These 3 lines are fields.
+    private int numWindows;
+    private String color;
+    
+    // non-arg constructor or Default constructor
+    public House()
+    {
+        numStories=5;
+        numWindows=6;
+        color="Green";
+    }
+    
+    // paramatrized constructor
+    public House(int numStories,int numWindows,String color)
+    {
+        this.numStories= numStories;
+        this.numWindows= numWindows;
+        this.color= color;
+    }
+    
+    
+    public int getNumStories()
+    {
+        return numStories;
+    }
+    public void setNumStories(int numStories)
+    {
+        this.numStories=numStories;
+    }
+    
+    public int getNumWindows()
+    {
+        return numWindows;
+    }
+    public void setNumWindows(int numWindows)
+    {
+        this.numWindows=numWindows;
+    }
+    
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color=color;
+    }
+    
+}
