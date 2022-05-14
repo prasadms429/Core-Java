780b9f41ed02a1b370cac1f67b67902762296d5b
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-15 01:02:10 +0530

    Create TwiceData.java

---
+++ 7) File IO and Exceptions/File input and output/TwiceData/TwiceData.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class TwiceData
+{
+	public static void main(String[] args) {
+		Scanner infile;
+		PrintWriter pw;
+		
+		try{
+		    infile=new Scanner(new File("nums.txt"));
+		    pw=new PrintWriter("twice_nums.txt");
+		    int inputNum;
+		    while(infile.hasNext()){
+		        inputNum=infile.nextInt();
+		        inputNum *= 2;
+		        pw.println(inputNum);
+		    }
+		    infile.close();
+		    pw.close();
+		}
+		catch(FileNotFoundException ex){
+		    System.out.println(ex.getMessage());
+		}
+	}
+}
